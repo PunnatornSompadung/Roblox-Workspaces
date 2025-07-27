f2067b288693f277da100054f487969da6fcf344
Noorth <38935.prc@gmail.com>
2025-07-28 03:07:21 +0700

    Add files via upload

---
+++ Model_Script/Flicker Light/Flicker light.lua
@@ -0,0 +1,8 @@
+while true do
+	wait(0.1)
+	script.Parent.Brightness = 2
+	wait(0.1)
+	script.Parent.Brightness = 0
+end
+
+-- script.Parent.Brightness = 0.7 <-- สามารถเปลี่ยนค่าได้ (ระดับแสง)
---
+++ Model_Script/Flicker Light/Flicker part.lua
@@ -0,0 +1,9 @@
+while true do
+	wait(0.1)
+	script.Parent.Light.Transparency = 0
+	wait(0.1)
+	script.Parent.Light.Transparency = 0.7
+end
+
+ -- // ชื่อ Part ของคุณ ตัวอย่าง (script.Parent.(Part <----).Transparency) //
+-- Light.Transparency = 0.7 <-- สามารถเปลี่ยนค่าได้ 
